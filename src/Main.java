a796edc07f427d17e3f8575791d8a9ee77dac5bc
JohelMena <jomenabe@est.utn.ac.cr>
2024-06-12 17:26:55 -0600

    inico de examen

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*=================================================================================
+Study Center....: Universidad Tecnica Nacional
+Campus..........: Paccifico (JRMP)
+College career..: Ingenieria en Tecnologias de Informacion
+Period..........: 2C-2024
+Course..........: ITI-221 - Programacion I
+Document........: class_01 - main.java
+Goals...........: Make the exam and have a good note
+Professor.......: Jorge Ruiz (york)
+Student.........:  Johel Mena
+=================================================================================*/
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
